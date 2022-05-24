f39e4d8cf8375df734917a01d7059ddb5da0c0de
Jan Deschacht <jandesch@student.arteveldehs.be>
2022-05-24 23:27:53 +0200

    CMS setup

---
+++ src/api/steplist/routes/steplist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * steplist router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::steplist.steplist');
---
+++ src/api/steplist/services/steplist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * steplist service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::steplist.steplist');
