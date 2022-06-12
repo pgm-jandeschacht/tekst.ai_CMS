27ae2db26483a24db91ece2bc07dcaee9f1ca97c
Jan Deschacht <jandesch@student.arteveldehs.be>
2022-06-12 18:09:15 +0200

    Changed cms credentials

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: `${process.env.DATABASE_HOST || config.host}`,
+      port: `${ process.env.DATABASE_PORT || config.port}`,
+      database: `${ process.env.DATABASE_NAME || config.database}`,
+      user: `${ process.env.DATABASE_USERNAME || config.user}`,
+      password: `${ process.env.DATABASE_PASSWORD || config.password}`,
       ssl: {
         rejectUnauthorized: false
       },
