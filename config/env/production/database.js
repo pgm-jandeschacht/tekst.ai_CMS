c33da7517d39d90d40585cbccd0b9ff6b970a3a2
Jan Deschacht <jandesch@student.arteveldehs.be>
2022-06-12 18:28:25 +0200

    Test

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,20 @@
-// const parse = require('pg-connection-string').parse;
-// const config = parse(process.env.DATABASE_URL);
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: `${process.env.DATABASE_HOST || config.host}`,
-//       port: `${ process.env.DATABASE_PORT || config.port}`,
-//       database: `${ process.env.DATABASE_NAME || config.database}`,
-//       user: `${ process.env.DATABASE_USERNAME || config.user}`,
-//       password: `${ process.env.DATABASE_PASSWORD || config.password}`,
-//       ssl: {
-//         rejectUnauthorized: false
-//       },
-//     },
-//     debug: false,
-//   },
-// });
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: `${config.DATABASE_HOST || config.host}`,
+        port: `${ config.DATABASE_PORT || config.port}`,
+        database: `${ config.DATABASE_NAME || config.database}`,
+        user: `${ config.DATABASE_USERNAME || config.user}`,
+        password: `${ config.DATABASE_PASSWORD || config.password}`,
+      },
+      options: {
+        rejectUnauthorized: false
+      }
+    }
+  },
+});
