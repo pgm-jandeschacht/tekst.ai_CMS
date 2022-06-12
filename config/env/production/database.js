f099020cffcc9158b4622375a5094f2a727199fd
Jan Deschacht <jandesch@student.arteveldehs.be>
2022-06-12 18:38:44 +0200

    Test 3

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        user: config.user,
-        password: config.password,
+        host: config.process.env.DATABASE_HOST,
+        port: config.process.env.DATABASE_PORT,
+        database: config.process.env.DATABASE_NAME,
+        user: config.process.env.DATABASE_USERNAME,
+        password: config.process.env.DATABASE_PASSWORD,
       },
       options: {
         rejectUnauthorized: false
