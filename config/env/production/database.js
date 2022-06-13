491a546d2dc28ddbf83cf37858fdb14b67991013
Jan Deschacht <jandesch@student.arteveldehs.be>
2022-06-14 00:46:43 +0200

    Test 6

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,21 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        database: process.env.DATABASE_NAME,
-        user: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
+        host: config.DATABASE_HOST,
+        port: config.DATABASE_PORT,
+        database: config.DATABASE_NAME,
+        user: config.DATABASE_USERNAME,
+        password: config.DATABASE_PASSWORD,
       },
       options: {
-        rejectUnauthorized: false
+        ssl: true,
+        rejectUnauthorized: false,
       }
     }
   },
