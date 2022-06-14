1f9d6a5ced060b53321dfd8551ab372761208a19
Jan Deschacht <jandesch@student.arteveldehs.be>
2022-06-14 10:53:22 +0200

    Tweak to readme and cleanup

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,3 @@
-// const parse = require('pg-connection-string').parse;
-// const config = parse(process.env.DATABASE_URL);
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connection: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'postgres',
-//         host: config.DATABASE_HOST,
-//         port: config.DATABASE_PORT,
-//         database: config.DATABASE_NAME,
-//         user: config.DATABASE_USERNAME,
-//         password: config.DATABASE_PASSWORD,
-//       },
-//       options: {
-//         ssl: true,
-//         rejectUnauthorized: false,
-//       }
-//     }
-//   },
-// });
-
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
